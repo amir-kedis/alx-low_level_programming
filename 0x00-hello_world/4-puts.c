940c9c2d42d3440b75918bfb1318e121b050d74c
amir_kedis <amir.kedis@gmail.com>
2023-07-13 01:14:57 -0700

    Solve task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 always - exit without errors
+ *
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
