5e4062f7ef4a396e0d129680c3b6734b089b4764
amir-kedis <amir.kedis@gmail.com>
2023-07-22 22:00:08 +0300

    0x01 C - Variables - task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints the numbers from 1 to 10
+*
+* Return: 0 always
+*/
+int main(void)
+{
+	int aDigit;
+
+	for (aDigit = '0'; aDigit <= '9'; aDigit++)
+	{
+		putchar(aDigit);
+	}
+	putchar('\n');
+
+	return (0);
+}
