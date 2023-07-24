bef0c65bac7ee978647872f90c386a0427e0af91
amir-kedis <amir.kedis@gmail.com>
2023-07-24 14:04:19 +0300

    0x02 C - Functions - task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - prints from n to 98
+* @n: starting number
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("%d\n", i);
+}
