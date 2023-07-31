c4c634db4c54e56b6878aa9f282dd180155e08de
amir-kedis <amir.kedis@gmail.com>
2023-07-31 21:10:37 +0300

    0x07 C - Pointers - task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: String to be scanned.
+ * @needle: String containing the sequence of characters to match.
+ * Return: Pointer to the beginning of the located substring, or NULL if the
+ * substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0' && haystack[i + j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+
+	return (NULL);
+}
