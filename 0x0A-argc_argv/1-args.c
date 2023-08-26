53762a12cbed900c1d397abcb4a7dd67e36f29f6
amir-kedis <amir.kedis@gmail.com>
2023-08-26 13:03:30 +0300

    0x0A C - argc argv - task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
