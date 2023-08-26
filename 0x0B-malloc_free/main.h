c34451d2503cd4861deda0a05133ecc891a9a1ee
amir-kedis <amir.kedis@gmail.com>
2023-08-26 14:29:41 +0300

    0x0B C - malloc free - task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 /* Header files */
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* Prototypes */
 int _putchar(char c);
+char *create_array(unsigned int size, char c);
 
 #endif
