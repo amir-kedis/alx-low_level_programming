c344e901b116bcd0e2591ca32b7c7caee536de2b
amir-kedis <amir.kedis@gmail.com>
2023-08-28 13:17:58 +0300

    0x0B C - more malloc free

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+In this project we will more about memory allocation in C (malloc, calloc, ralloc, free).
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* ------ Includes ------ */
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ------ Prototypes ------ */
+void *malloc_checked(unsigned int b);
+
+#endif
