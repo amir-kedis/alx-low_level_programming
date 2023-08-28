75585c3d4e024b052a53a6c423b5dc8abd37c04f
amir-kedis <amir.kedis@gmail.com>
2023-08-28 19:47:15 +0300

    0x0F C - Function Pointers - task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful, 98 if number of arguments is wrong,
+ * 99 if operator is incorrect, 100 if divide by 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", f(a, b));
+	return (0);
+}
