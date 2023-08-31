e70d9284e6b5b9d283260a5d88321d79c055bd2a
amir-kedis <amir.kedis@gmail.com>
2023-08-31 11:18:05 +0300

    0x14 C - Bit Manipulation - task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+#define BITS_IN_ULINT 8 * 8
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, flag = 0;
+
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (i = BITS_IN_ULINT; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			flag = 1;
+		if (flag)
+			_putchar((n >> i) & 1 ? '1' : '0');
+	}
+}
+
+/* This code starts from the most significant bit and prints the value of the
+ * bit at each position. It uses a flag to determine when to print the value of
+ * the bit. The flag is set when the first 1 is encountered. */
